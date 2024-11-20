b4e279106003496895b94a6a4d9003149a40fd75
siiion <siwonjeon234@gmail.com>
2024-11-20 17:46:04 +0900

    Mod: 마이크 센서 연결 확인 코드로 수정

--- outdoor/vibration_sensor.py
+++ outdoor/vibration_sensor.py
@@ -1,17 +1,17 @@
 import RPi.GPIO as GPIO
 import time
 
-SENSOR_PIN = 18  # SW-420의 DO 핀 연결
+SENSOR_PIN = 18  
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(SENSOR_PIN, GPIO.IN)
 
 try:
     while True:
-        if GPIO.input(SENSOR_PIN):  # 진동 감지
-            print("Vibration detected!")  # 진동 감지 시 메시지 출력
+        if GPIO.input(SENSOR_PIN):  
+            print("Vibration detected!") 
         else:
-            print("No vibration detected.")  # 진동 미감지 시 메시지 출력
-        time.sleep(0.1)  # 짧은 주기로 상태 확인
+            print("No vibration detected.")  
+        time.sleep(0.1)  
 except KeyboardInterrupt:
-    GPIO.cleanup()  # 프로그램 종료 시 GPIO 핀 정리
+    GPIO.cleanup()  
